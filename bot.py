902dfb6cdbc58e1042e319721051504b37d06cb6
ATEZ <shieldnet64@gmail.com>
2019-05-20 16:41:44 +0900

    Refactoring

--- bot.py
+++ bot.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 
 from time import sleep
-
+import sys
 from youtubechat import YoutubeLiveChat, get_live_chat_id_for_broadcast_id, get_live_chat_id_for_stream_now
 
-broadcast_id = 'zDWUNqe3alo'
+argv = sys.argv
+
+if len(argv) is not 2:
+    print("Use like bot.py <broadcast_id>")
+    exit(-1)
+
+broadcast_id = str(argv[1])
+
+print(broadcast_id)
 
 livechat_id = get_live_chat_id_for_broadcast_id(broadcast_id,"oauth_creds")
 print(livechat_id)
+
 chat_obj = YoutubeLiveChat("oauth_creds", [livechat_id])
 
 
@@ -15,7 +24,7 @@ def respond(msgs, chatid):
     for msg in msgs:
         print(msg)
         if msg.message_text[0] is '!':
-            chat_obj.send_message("DDOKDDOK : "+msg.message_text, chatid)
+            chat_obj.send_message("BOT: " +msg.message_text, chatid)
             msg.delete()
 
 try:
