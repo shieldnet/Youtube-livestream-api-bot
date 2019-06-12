07f659757864489bd0cf8f54fcd8cc19cd2d34e8
ATEZ <shieldnet64@gmail.com>
2019-06-12 21:11:54 +0900

    Uptime Ended, Close #2

--- bot.py
+++ bot.py
@@ -20,22 +20,27 @@ print(livechat_id)
 chat_obj = YoutubeLiveChat("oauth_creds", [livechat_id])
 
 
+# Command
 def uptime(chatid):
     response = '방송이 시작된 지, '+get_broadcast_start_time(broadcast_id,"oauth_creds") + '이 지났습니다.';
     chat_obj.send_message(response, chatid)
 
+
+#Parsing Commands
 def respond(msgs, chatid):
     for msg in msgs:
         print(msg)
         if msg.message_text.find('!업타임') != -1:
             uptime(chatid)
-        elif msg.message_text[0] == '!':
-            chat_obj.send_message("BOT: " +msg.message_text, chatid)
+        elif msg.message_text.find('!앵무새') != -1:
+            chat_obj.send_message("DDOKDDOK "+msg.message_text[4:], chatid)
             msg.delete()
         else:
             print(msg)
 
 
+
+# Enroll Bot to thread
 try:
     chat_obj.start()
     chat_obj.subscribe_chat_message(respond)
